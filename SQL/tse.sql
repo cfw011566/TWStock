ca0cf3c3d3e5da2893f695350fa7f57ad1904be8
Ching-Feng Wang <cf.wang@ampacscorp.com>
2017-09-27 09:10:16 +0800

    add margin purchase and short sale

--- SQL/tse.sql
+++ SQL/tse.sql
@@ -61,3 +61,23 @@ CREATE TABLE indices (
 	UNIQUE (trade_datetime, security_code)
 );
 
+
+-- Margin Transaction 融資Purchase on Margin/融券Short Sale
+
+CREATE TABLE daily_margin_short (
+	trade_date		date,
+	security_code	varchar,
+	margin_new			numeric,	-- 融資買進
+	margin_redemption	numeric,	-- 融資賣出
+	margin_outstanding	numeric,	-- 現金償還
+	margin_remain		numeric,	-- 餘額
+	margin_limit		numeric,	-- 限額
+	short_redemption	numeric,	-- 融券買進
+	short_new			numeric,	-- 融券賣出
+	short_outstanding	numeric,
+	short_remain		numeric,
+	short_limit			numeric,
+	margin_and_short	numeric,	-- 資券互抵
+	UNIQUE (trade_date, security_code)
+);
+
